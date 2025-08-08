753f1ac6fb687fad92b3b0024036662a781d902d
Vijay Perugu <124055443+Vijayperugu@users.noreply.github.com>
2025-08-08 15:11:06 +0530

    VIJAYP0001 – Added odd/even checker in Utility.js

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,11 @@
+function vijay() {
+    console.log("I am Vijay");
+    const num = 10;
+    if (num % 2 === 0) {
+        console.log(num + " is Even");
+    } else {
+        console.log(num + " is Odd");
+    }
+}
+
+vijay(); 
